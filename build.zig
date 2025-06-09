51d3d30c0e9d429c0ba3a7f38e8c0ab280203dbd
Jonathan Marler <johnnymarler@gmail.com>
2025-06-09 15:06:06 -0600

    zig update 0.14.1

--- build.zig
+++ build.zig
@@ -1,36 +1,37 @@
 const std = @import("std");
-const GitRepoStep = @import("GitRepoStep.zig");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "hacknes",
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.addCSourceFiles(.{
+        .files = &.{
+            "hacknes.cpp",
+            "platformPosix.cpp",
+            "cartridge.cpp",
+            "cpu.cpp",
+            "ppu.cpp",
+            "graphics/dummy.cpp",
+            "audio/dummy.cpp",
+        },
+    });
+    exe.linkLibCpp();
 
     {
-        const exe = b.addExecutable("hacknes", null);
-        exe.setTarget(target);
-        exe.setBuildMode(mode);
-        exe.addCSourceFiles(&[_][]const u8 {
-            "hacknes.cpp", "platformPosix.cpp", "cartridge.cpp", "cpu.cpp", "ppu.cpp", "graphics/dummy.cpp", "audio/dummy.cpp",
-        }, &[_][]const u8 {});
-        exe.linkLibCpp();
-        exe.install();
-        
-        const run_cmd = exe.run();
-        run_cmd.step.dependOn(b.getInstallStep());
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
+        const install_exe = b.addInstallArtifact(exe, .{});
+        b.getInstallStep().dependOn(&install_exe.step);
+        const run = b.addRunArtifact(exe);
+        run.step.dependOn(&install_exe.step);
+        if (b.args) |args| run.addArgs(args);
+        b.step("run", "").dependOn(&run.step);
+    }
 
-        const run_step = b.step("run", "Run the app");
-        run_step.dependOn(&run_cmd.step);
-    }
-    {
-        const test_rom_repo = GitRepoStep.create(b, .{
-            .url = "https://github.com/christopherpow/nes-test-roms",
-            .branch = null,
-            .sha = "95d8f621ae55cee0d09b91519a8989ae0e64753b",
-            .fetch_enabled = true,
-        });
-        b.step("test-roms", "fetch test rom repo").dependOn(&test_rom_repo.step);
-    }
+    const nes_test_roms_dep = b.dependency("nes_test_roms", .{});
+    _ = nes_test_roms_dep;
+    // TODO: add some tests?
 }
